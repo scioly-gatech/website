0c0492afc9811cae9ced6aa1fa3ddcc34f1dd8cc
Julie Young <julieyoung@lawn-143-215-52-220.lawn.gatech.edu>
2023-09-20 20:26:04 -0400

    added some exec pictures

--- src/app/aboutUs/page.tsx
+++ src/app/aboutUs/page.tsx
@@ -15,21 +15,21 @@ const play = Lora({
 export default function MeetTeam() {
 
   const profiles = [
-    {id: 1, name: "Allen Chang", role: "President", imgRoute: "/images/blankProfile.jpeg", info: "ANYTHING ANYTHING ANYTHING"},
-    {id: 2, name: "Sparsh Kudrimoti", role: "Vice President", imgRoute: "/images/blankProfile.jpeg", info: "ANYTHING ANYTHING ANYTHING"},
+    {id: 1, name: "Allen Chang", role: "President", imgRoute: "/images/execPics/Allen-cropped.jpg", info: "ANYTHING ANYTHING ANYTHING"},
+    {id: 2, name: "Sparsh Kudrimoti", role: "Vice President", imgRoute: "/images/execPics/Sparsh.jpeg", info: "ANYTHING ANYTHING ANYTHING"},
     {id: 3, name: "Vidit Pokharna", role: "Finance Officer", imgRoute: "/images/blankProfile.jpeg", info: "ANYTHING ANYTHING ANYTHING"},
-    {id: 4, name: "Siva Appana", role: "Build Director", imgRoute: "/images/blankProfile.jpeg", info: "ANYTHING ANYTHING ANYTHING"},
-    {id: 5, name: "Sean Ru", role: "Build Director", imgRoute: "/images/blankProfile.jpeg", info: "ANYTHING ANYTHING ANYTHING"},
-    {id: 6, name: "Dylan Chandra", role: "Build Director", imgRoute: "/images/blankProfile.jpeg", info: "ANYTHING ANYTHING ANYTHING"},
+    {id: 4, name: "Siva Appana", role: "Build Director", imgRoute: "/images/execPics/Siva.jpg", info: "ANYTHING ANYTHING ANYTHING"},
+    {id: 5, name: "Sean Ru", role: "Build Director", imgRoute: "/images/execPics/Sean-cropped.jpg", info: "ANYTHING ANYTHING ANYTHING"},
+    {id: 6, name: "Dylan Chandra", role: "Build Director", imgRoute: "/images/execPics/Dylan-cropped.jpg", info: "ANYTHING ANYTHING ANYTHING"},
     {id: 7, name: "Syaam Khandaker", role: "Exam Director", role2: "Physics", imgRoute: "/images/blankProfile.jpeg", info: "ANYTHING ANYTHING ANYTHING"},
-    {id: 8, name: "Tonya Melnikova", role: "Exam Director/Chemistry", imgRoute: "/images/blankProfile.jpeg", info: "ANYTHING ANYTHING ANYTHING"},
-    {id: 9, name: "Cade Jones", role: "Exam Director/Biology", imgRoute: "/images/blankProfile.jpeg", info: "ANYTHING ANYTHING ANYTHING"},
-    {id: 10, name: "Shriya Nayak", role: "Exam Director/Earth Science", imgRoute: "/images/blankProfile.jpeg", info: "ANYTHING ANYTHING ANYTHING"},
+    {id: 8, name: "Tonya Melnikova", role: "Exam Director/Chemistry", imgRoute: "/images/execPics/Tonya-cropped.jpg", info: "ANYTHING ANYTHING ANYTHING"},
+    {id: 9, name: "Cade Jones", role: "Exam Director/Biology", imgRoute: "/images/execPics/Cade.jpg", info: "ANYTHING ANYTHING ANYTHING"},
+    {id: 10, name: "Shriya Nayak", role: "Exam Director/Earth Science", imgRoute: "/images/execPics/Shriya-cropped.JPG", info: "ANYTHING ANYTHING ANYTHING"},
     {id: 11, name: "Ashika Srivastava", role: "Logistics/Competitor Logistics", imgRoute: "/images/blankProfile.jpeg", info: "ANYTHING ANYTHING ANYTHING"},
-    {id: 12, name: "Morgan Duvall", role: "Logistics/Volunteer Logistics", imgRoute: "/images/blankProfile.jpeg", info: "ANYTHING ANYTHING ANYTHING"},
+    {id: 12, name: "Morgan Duvall", role: "Logistics/Volunteer Logistics", imgRoute: "/images/execPics/Morgan.JPG", info: "ANYTHING ANYTHING ANYTHING"},
     {id: 13, name: "Jason Katz", role: "Logistics/Tournament Logistics", imgRoute: "/images/blankProfile.jpeg", info: "ANYTHING ANYTHING ANYTHING"},
     {id: 14, name: "Harsha Gaddipati", role: "Public Relations", imgRoute: "/images/blankProfile.jpeg", info: "ANYTHING ANYTHING ANYTHING"},
-    {id: 15, name: "Julie Young", role: "Technology", imgRoute: "/images/blankProfile.jpeg", info: "ANYTHING ANYTHING ANYTHING"},
+    {id: 15, name: "Julie Young", role: "Technology", imgRoute: "/images/execPics/Julie-cropped.jpg", info: "ANYTHING ANYTHING ANYTHING"},
   ]
 
   //Stagger animation below was modified from Leigh Halliday's YouTube tutorial titled 'Staggered Animations in React with Framer Motion in 5 Minutes'. The video can be found at this link: https://www.youtube.com/watch?v=u_95SPKE6vg&ab_channel=LeighHalliday
--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -28,7 +28,7 @@ export default function Footer() {
       </ul>
     </nav>
       <p className="left-1/2 text-center">For more information about Science Olympiad, please visit <a href="https://www.soinc.org/" target="_blank" className="underline text-white">https://www.soinc.org/ </a></p>
-      <p className="left-1/2 text-center">Created by <span className="text-black font-bold">Science Olympiad @ Georgia Tech </span>{curr}</p>
+      <p className="left-1/2 text-center">Created by <span className="text-yellow-300 font-bold">Science Olympiad @ Georgia Tech </span>{curr}</p>
       <ul id="socialMedia" className="flex flex-row">
         <li id="instagram" className="m-2">
           <a href="https://www.instagram.com/gtscioly/" target="_blank" className="text-3xl hover:brightness-150"><FaInstagramSquare /></a>
