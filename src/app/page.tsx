6a469fd0eba08fcb091c8c6311dfb4c681c22065
Julie Young <113480519+jyoung369@users.noreply.github.com>
2023-12-22 13:25:42 -0700

    Removed some unnecessary code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,7 +52,7 @@ export default function Home() {
           animate={{opacity:1}}
           transition={{duration:4}}
         >
-        <h1 className={`text-6xl text-center border-4 borer-white text-white lg:p-20 m-4 mx-12 w-screen, ${play.className}`}><span className="">Science Olympiad @ Georgia Tech </span></h1>
+        <h1 className={`text-6xl text-center border-4 borer-white text-white lg:p-20 m-4 mx-12 w-screen, ${play.className}`}><span>Science Olympiad @ Georgia Tech </span></h1>
       </motion.div>
       </AnimatePresence>
     </div>
