030aa19e542fbcacfaf470ea456d15b75af5350a
Julie Young <113480519+jyoung369@users.noreply.github.com>
2024-01-08 18:44:33 -0500

    Updates to Contact Us page

--- src/app/contactUs/page.tsx
+++ src/app/contactUs/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {FaInstagramSquare} from "react-icons/fa"
 import { Lora } from 'next/font/google'
 import Image from 'next/image'
 import Script from 'next/script'
@@ -26,13 +27,18 @@ export default function ContactUs() {
           });
       `}
       </Script>
-    <div className="bg-slate-200 dark:bg-black h-full">
+    <div className="bg-amber-50 dark:bg-black h-full">
         <div id="all content" className="w-screen flex flex-col items-center">
           <div id="header" className="bg-[url('/images/contact/contact.jpg')] w-screen flex justify-center items-center h-48">
             <h1 className={`text-white font-bold border-8 p-4 m-16 text-5xl md:text-7xl text-center ${play.className} drop-shadow-titleShadow`}>Contact Us </h1>
           </div>
-        <p className="text-black dark:text-white border-4 text-center text-lg m-8 mx-72 px-4 pt-4">
-          If you have any questions or comments regarding competing, sponsoring, or anything else related to Science Olympiad at Georgia Tech, we would love to hear from you! Please do not hesitate to reach out to us at <a href="malito:scioly.gatech@gmail.com" className="underline font-semibold">scioly.gatech@gmail.com</a>.
+        <p className="text-black dark:text-white border-4 border-black bg-lightOrange text-center text-2xl m-8 mx-72 p-4 shadow-2xl shadow-darkOrange">
+          If you have any questions or comments regarding competing, sponsoring, or anything else related to Science Olympiad at Georgia Tech, we would love to hear from you! 
+          <br />
+          <br />
+         <span className="font-bold">Email: </span><a href="malito:scioly.gatech@gmail.com" className="hover:text-darkOrange">scioly.gatech@gmail.com</a>
+         <br />
+         <span className="font-bold">Instagram: </span><a href="https://www.instagram.com/gtscioly/" target="_blank" className="hover:text-darkOrange">@gtscioly</a>
         </p>
         <figure className="mb-8">
           <Image src="/images/officialLogo.png" alt="Picture of the Science Olympiad at Georgia Tech Logo" width="350" height="350"/>
