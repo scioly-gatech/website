1b6b5e5c66466e5198c408c17932808073923c95
rzhang630 <rzhang630@gatech.edu>
2024-02-14 18:06:52 -0500

    Fixed styling

--- __tests__/homepage.test.tsx
+++ __tests__/homepage.test.tsx
@@ -1,19 +1,19 @@
-import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
-import { mockIntersectionObserver } from 'jsdom-testing-mocks'
-import Home from '../src/app/page'
- 
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import { mockIntersectionObserver } from "jsdom-testing-mocks";
+import Home from "../src/app/page";
+
 mockIntersectionObserver();
 
 // Mock Carousel element to prevent Embla dependency issues
 jest.mock("../src/app/components/Carousel");
 
-describe('Home page', () => {
-  it('renders a heading', () => {
-    render(<Home />)
- 
-    const heading = screen.getByRole('heading', { level: 1 })
- 
-    expect(heading).toBeInTheDocument()
-  })
-})
+describe("Home page", () => {
+  it("renders a heading", () => {
+    render(<Home />);
+
+    const heading = screen.getByRole("heading", { level: 1 });
+
+    expect(heading).toBeInTheDocument();
+  });
+});
